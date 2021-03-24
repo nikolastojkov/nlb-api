0f3d2c6f02111893489f3f4cf01ef337b0bcd8be
Nikola Stojkov <stojkov@protonmail.ch>
2021-03-24 12:46:00 +0100

    some changes to the dev init command

--- src/Console/NLBKlikInit.php
+++ src/Console/NLBKlikInit.php
@@ -6,18 +6,48 @@ use Illuminate\Console\Command;
 
 class NLBKlikInit extends Command
 {
-    protected $signature = "nlbklik:init";
+    protected $signature = "nlbklik:dev:init";
 
     protected $description = "Initialize NLBKlik API. This will check if you have the NLBCLI installed and if you're correctly logged in.";
 
-    public function __construct()
-    {
-        parent::__construct();
-    }
-    
     public function handle()
     {
-        $this->info("TODO make this command work.");
+        $this->info("Welcome to the NLBKlik Laravel package. Please sign in to use this project for local development.");
+
+        $this->info("Please refer to https://github.com/whoeverest/nlbcli if you need more information about the logging/sessions logic.");
+
+        if ($this->confirm("Do you have NLBCLI installed on your system?", true))
+        {
+            $this->askForLogin();
+        }
+        else
+        {
+            if ($this->confirm("Would you like us to automatically install NLBCLI for you?", true))
+            {
+               $this->info("TODO: Automatically install NLBCLI. Do checks like is git/python/pip installed etc.");
+            }
+            else
+            {
+                $this->info("Run this command again when you have NLBCLI installed!");
+            }
+        }
+    }
+
+    private function askForLogin()
+    {
+        if ($this->confirm("Do you want to save your login credentials locally?", true))
+        {
+            $nlbcliOutput = [];
+
+            //exec("nlbcli login", $nlbcliOutput);
+
+            // TODO: Wait for NLBCLI issue #13 to finish this command with logging in etc.
+        }
+        else
+        {
+            // TODO: Find a way to fix.
+            $this->info("Using this package without locally stored credentials is currently not supported. Watch issue # for updates!");
+        }
     }
 }
 
